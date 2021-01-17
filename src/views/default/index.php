abde54520dc142d0978a131364a8137494e27c16
Arda Talu <arda.starkiler@gmail.com>
2021-01-17 04:29:38 +0300

    project updated

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -1,1 +1,44 @@
-<?php echo "Module ext Ok " ?>
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\computer\models\ComputerSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Computers';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="computer-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Computer', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'type',
+            'brand',
+            'model',
+            'cpu',
+            //'gpu',
+            //'ram',
+            //'created_at',
+            //'updated_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
